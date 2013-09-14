b8437aba1ae518351247fdc3f32889f3d6406ed4
Michael Jank <michael.jank@yahoo.com>
2013-09-14 12:53:24 +0200

    first jsf

---
+++ src/at/HelloBean.java
@@ -0,0 +1,18 @@
+package at;
+
+import javax.faces.bean.*;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
